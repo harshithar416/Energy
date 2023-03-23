6e9a4f9e8972a265b5141d21efaaf901d118cfbc
Harshitha Ramesh <DELL@MacBook-Pro.local>
2023-03-22 18:42:55 -0700

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,31 @@
 import './Home.css';
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
 
 class Home extends React.Component {
+    constructor(props) {    
+        super(props);
+        this.state = {      
+            selectedNode: 0,
+            displayedBanner: 0      
+        }; 
+    }   
+    componentDidMount() {
+        this.bannerChange() 
+    }
+    nodeOnClick(index) {
+        this.setState({selectedNode: index})
+    }
+    bannerChange() {
+        setTimeout(() => {
+            this.setState({displayedBanner: Math.floor(Math.random() * 3)})
+            this.bannerChange()
+        }, 3000);
+    }
     render() {
+        const nodes = ["SNJSEA_1_N101", "SNJOSEB_1_N013", "CATALYST_7_N002", "FMC_1_N003", "CSCGNRA2_7_N101", "EVRGREEN_1_N019", "MONTAGUE_1_N007", "MILPITAS_1_N008", "DIXONLD_1_N008", "LAWRENCE_1_N006"]
+
         return (
             <div id="home">
                 <header className='navbar'>
@@ -16,75 +35,50 @@ class Home extends React.Component {
                 </header>
                 <div className="journey_cards_section">
                     <div className="journey_card">
-                        <Card sx={{ minWidth: 275 }}>
-                            <img src="energy_price.jpeg" className="energy_price"></img>
+                        <Card sx={[{ minWidth: 275 , height: '100%'}]}>
+                            <img src={"banners/"+this.state.displayedBanner+ ".jpeg"} className="energy_price"></img>
                         </Card>
                     </div>
                     <div className="main_section">
                         <div className="nodes_section">
                             <Card sx={{ minWidth: 275 }} className="nodes_buttons">
-                                <button className="node-button" onClick={() => { }}>
-                                    SNJSEA_1_N101
+                            {nodes.map((node, index) => (
+                                <button key={index} className="node-button" onClick={() => { this.nodeOnClick(index)}}>
+                                    {node}
                                 </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    SNJOSEB_1_N013
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    CATALYST_7_N002
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    FMC_1_N003
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    CSCGNRA2_7_N101
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    EVRGREEN_1_N019
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    MONTAGUE_1_N007
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    MILPITAS_1_N008
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    DIXONLD_1_N008
-                                </button>
-                                <button className="node-button" onClick={() => { }}>
-                                    LAWRENCE_1_N006
-                                </button>
+                            ))}
                             </Card>
                         </div>
                         <div className="main_body">
                             <div className="stats_section">
                                 <Card sx={{ minWidth: 100 }} className="stat_card">
-                                    <span class="overview-icon current-demand">
-                                        <span class="overview-large-number">18,834 <span>MW</span></span>
-                                        <span class="overview-large-label">Current demand</span>
+                                    <span className="overview-icon current-demand">
+                                        <span className="overview-large-number">18,834 <span>MW</span></span>
+                                        <span className="overview-large-label">Current demand</span>
                                     </span>
                                 </Card>
                                 <Card sx={{ minWidth: 100 }} className="stat_card">
-                                    <span class="overview-icon forecasted-peak">
-                                        <span class="overview-large-number">26,199 <span>MW</span></span>
-                                        <span class="overview-large-label">Forecasted peak</span>
+                                    <span className="overview-icon forecasted-peak">
+                                        <span className="overview-large-number">26,199 <span>MW</span></span>
+                                        <span className="overview-large-label">Forecasted peak</span>
                                     </span>
                                 </Card>
                                 <Card sx={{ minWidth: 100 }} className="stat_card">
-                                    <span class="overview-icon current-demand">
-                                        <span class="overview-large-number">18,834 <span>MW</span></span>
-                                        <span class="overview-large-label">Current demand</span>
+                                    <span className="overview-icon current-demand">
+                                        <span className="overview-large-number">18,834 <span>MW</span></span>
+                                        <span className="overview-large-label">Current demand</span>
                                     </span>
                                 </Card>
                                 <Card sx={{ minWidth: 100 }} className="stat_card">
-                                    <span class="overview-icon forecasted-peak">
-                                        <span class="overview-large-number">26,199 <span>MW</span></span>
-                                        <span class="overview-large-label">Forecasted peak</span>
+                                    <span className="overview-icon forecasted-peak">
+                                        <span className="overview-large-number">26,199 <span>MW</span></span>
+                                        <span className="overview-large-label">Forecasted peak</span>
                                     </span>
                                 </Card>
                             </div>
                             <div className="graph_section">
                                 <Card sx={{ minWidth: 275 }}>
-                                    <img src="sample_graph.png" className="sample_graph"></img>
+                                    <img src={"graphs/"+this.state.selectedNode+".png"} className="sample_graph"></img>
                                 </Card>
                             </div>
                         </div>
